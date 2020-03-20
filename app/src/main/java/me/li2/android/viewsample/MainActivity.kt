19377dfece2b02061472597e8313693a0494740e
Weiyi Li <weiyi.just2@gmail.com>
2020-03-21 10:54:48 +1300

    Init project

---
+++ app/src/main/java/me/li2/android/viewsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.li2.android.viewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
