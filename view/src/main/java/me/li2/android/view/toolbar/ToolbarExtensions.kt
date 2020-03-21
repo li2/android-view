295ab1f18c15a6328f52b2c9d3a7efca52cc829c
Weiyi Li <weiyi.just2@gmail.com>
2020-03-21 19:22:42 +1300

    Added ToolbarExtensions

---
+++ view/src/main/java/me/li2/android/view/toolbar/ToolbarExtensions.kt
@@ -0,0 +1,46 @@
+@file:JvmName("ToolbarExtensions")
+package me.li2.android.view.toolbar
+
+import android.app.Activity
+import android.graphics.PorterDuff
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
+
+fun Activity.setToolbar(toolbar: Toolbar, title: String = "", @DrawableRes iconId: Int? = null) {
+    (this as? AppCompatActivity)?.apply {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setTitle(title)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+        iconId?.run { toolbar.setNavigationIcon(this) }
+    }
+}
+
+fun AppBarLayout.setCollapsingListener(onCollapsed: (Boolean) -> Unit) {
+    addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+        onCollapsed(abs(verticalOffset) - appBarLayout.totalScrollRange == 0)
+    })
+}
+
+fun Toolbar.setIconColor(@ColorInt color: Int) {
+    setBackIconColor(color)
+    setMenuIconColor(color)
+}
+
+fun Toolbar.setMenuIconColor(@ColorInt color: Int) {
+    for (i in 0 until menu.size()) {
+        val icon = menu.getItem(i).icon
+        icon?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+    }
+}
+
+fun Toolbar.setBackIconColor(@ColorInt color: Int) {
+    navigationIcon?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+}
