503349676a507be4d1ccd8a099f8d006b0a87606
weiyi.li <weiyi.li@vista.co>
2020-03-21 18:57:03 +1300

    Added TextView, EditTextView bindings and extensions.

--- view/src/main/java/me/li2/android/view/placeholder.kt
+++
@@ -1,2 +0,0 @@
-package me.li2.android.view
-
---
+++ view/src/main/java/me/li2/android/view/textview/EditTextViewExtensions.kt
@@ -0,0 +1,79 @@
+/*
+ * Created by weiyi on 2019-10-28.
+ * https://github.com/li2
+ */
+package me.li2.android.view.textview
+
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.annotation.StringRes
+import com.google.android.material.textfield.TextInputLayout
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.Observable
+import me.li2.android.common.number.orZero
+import me.li2.android.common.rx.mapToString
+import java.util.concurrent.TimeUnit
+
+fun EditText.errorCheckingTextChanges(
+        textInputLayout: TextInputLayout,
+        @StringRes errorMessageId: Int,
+        isValid: (String) -> Boolean): Observable<String> {
+    return textChanges().mapToString().doOnNext { input ->
+        if (input.isNotEmpty()) {
+            textInputLayout.error = if (isValid(input)) null else textInputLayout.context.getString(errorMessageId)
+        }
+    }
+}
+
+/**
+ * Return query text changes observable.
+ *
+ * - filter: to filter undesired text like blank text to avoid unnecessary API call.
+ * - debounce: to ignore the previous items in the given time and only emit the last one,
+ *             to avoid too much API calls.
+ * - distinctUntilChanged: to avoid the duplicate API call.
+ */
+fun EditText.queryTextChanges(): Observable<String> {
+    return textChanges()
+            .map { it.toString() }
+            .filter { it.isNotBlank() }
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+}
+
+/**
+ * Text changes observable which only be triggered by user input.
+ * To make it works, call [setTextProgrammatically] whenever change text programmatically.
+ *
+ * https://stackoverflow.com/a/25751207/2722270
+ */
+fun EditText.userTextChanges(): Observable<CharSequence> {
+    return Observable.create { emitter ->
+        textChanges().skipInitialValue().subscribe { text ->
+            if (tag == null) {
+                // means the change is from user, then emitter it
+                emitter.onNext(text)
+            }
+        }
+    }
+}
+
+fun EditText.setTextProgrammatically(text: CharSequence?) {
+    tag = "the arbitrary const tag is used to distinguish the textChanges() is by user or code"
+    setText(text)
+    setSelection(text?.length.orZero())
+    tag = null
+}
+
+fun EditText.onImeActionDoneClicks(): Observable<Unit> {
+    return Observable.create { emitter ->
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                emitter.onNext(Unit)
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
