1feb858322bbfbcd8a458198efc0cd4c11e9652a
weiyi.li <weiyi.li@vista.co>
2020-04-04 18:48:40 +1300

    Fixed LoadingOverlayView

--- view/src/main/java/me/li2/android/view/view/LoadingOverlay.kt
+++ view/src/main/java/me/li2/android/view/view/LoadingOverlay.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package me.li2.android.view.view
 
 import android.content.Context
@@ -12,32 +14,34 @@ import me.li2.android.view.toast.toast
  * A transparent overlay view shows on top of the screen to block user touch events,
  * and toasts "Loading…" when user touches on screen.
  * - This view is useful for waiting Api, just simply update its visibility base on loading status.
- * - You might want to change the toast message by app:toastMessage="@{}"
+ * - You might want to change the toast message by app:loadingOverlayToast="@{}"
  */
 class LoadingOverlayView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr), View.OnClickListener {
 
-    var toastMessage: String
+    var toastText: String? = null
 
     init {
         inflate(context, R.layout.loading_overlay_view, null)
         // with hard-coding elevation to make sure it CAN block all the touch events
         ViewCompat.setElevation(this, resources.getDimensionPixelSize(R.dimen.viewLib_loading_overlay_elevation) * 1.0f)
         setOnClickListener(this)
-        toastMessage = context.resources.getString(R.string.viewlib_loadingoverlay_loading)
+        toastText = context.resources.getString(R.string.viewlib_loadingoverlay_loading)
     }
 
     override fun onClick(v: View?) {
-        context.toast(toastMessage)
+        if (!toastText.isNullOrEmpty()) {
+            context.toast(toastText.orEmpty())
+        }
     }
 
     companion object {
         @JvmStatic
-        @BindingAdapter("app:toastMessage")
-        fun setUserAgentString(loadingOverlayView: LoadingOverlayView, toastMessage: String?) {
-            toastMessage?.let { loadingOverlayView.toastMessage = it }
+        @BindingAdapter("loadingOverlayToast")
+        fun setLoadingOverlayToast(loadingOverlayView: LoadingOverlayView, toastText: String?) {
+            loadingOverlayView.toastText = toastText
         }
     }
 }
