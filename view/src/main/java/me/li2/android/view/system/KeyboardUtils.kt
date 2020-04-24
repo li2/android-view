88cc6c98066a4e290b7f42ed458a74b31d06024c
Weiyi Li <weiyi.just2@gmail.com>
2020-04-24 17:55:17 +1200

    Keyboard visible changes

--- view/src/main/java/me/li2/android/view/system/KeyboardUtils.kt
+++ view/src/main/java/me/li2/android/view/system/KeyboardUtils.kt
@@ -7,6 +7,8 @@ package me.li2.android.view.system
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 
@@ -18,3 +20,17 @@ fun Activity.hideKeyboard() {
 }
 
 fun Fragment.hideKeyboard() = activity?.hideKeyboard()
+
+/**
+* Receiver: root view of the fragment or activity.
+*/
+fun View.observeKeyboardVisibleChanges(onVisibleChanges: (Boolean) -> Unit) {
+    this.viewTreeObserver.addOnGlobalLayoutListener {
+        val r = Rect()
+        this.getWindowVisibleDisplayFrame(r)
+        val screenHeight = this.rootView.height
+        val keyboardHeight = screenHeight - r.bottom
+        val visible = keyboardHeight > 0.2 * screenHeight
+        onVisibleChanges(visible)
+    }
+}
